73e5b5d506658d9a15a35fffcc669b7b47225abc
Yū Kobayashi <kb10uy@kb10uy.org>
2024-08-26 23:01:41 +0900

    Add SkirtRotationInfluence

---
+++ Editor/Modules/SkirtInfluenceApplier.cs
@@ -0,0 +1,67 @@
+#if KZT_NDMF
+
+using UnityEngine;
+using VRC.Dynamics;
+using VRC.SDK3.Dynamics.Constraint.Components;
+using nadena.dev.ndmf;
+using KusakaFactory.Zatools.Runtime;
+
+namespace KusakaFactory.Zatools.Modules
+{
+    internal sealed class SkirtInfluenceApplier : Pass<SkirtInfluenceApplier>
+    {
+        public override string QualifiedName => nameof(SkirtInfluenceApplier);
+        public override string DisplayName => "Apply skirt PhysBone influence";
+
+        protected override void Execute(BuildContext context)
+        {
+            var components = context.AvatarRootObject.GetComponentsInChildren<SkirtRotationInfluence>();
+            foreach (var component in components) Apply(context, component);
+        }
+
+        private void Apply(BuildContext context, SkirtRotationInfluence influenceSettings)
+        {
+            var chainsCount = influenceSettings.SkirtChains.Length;
+            var virtualStart = influenceSettings.CloseLoop ? chainsCount : 0;
+            var virtualEnd = influenceSettings.CloseLoop ? virtualStart + chainsCount + 1 : virtualStart + chainsCount;
+
+            for (int i = virtualStart; i < virtualStart + chainsCount; ++i)
+            {
+                var center = influenceSettings.SkirtChains[i % chainsCount];
+
+                // Rotation Constraint 用の親を挿入
+                var fakeParent = new GameObject($"{center.gameObject.name}_Parent");
+                var parentTransform = fakeParent.transform;
+                parentTransform.parent = center;
+                parentTransform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+                parentTransform.localScale = Vector3.one;
+                parentTransform.localPosition -= Vector3.up * influenceSettings.ParentOffsetDistance;
+                parentTransform.SetParent(center.parent, true);
+                center.transform.SetParent(parentTransform, true);
+
+                // 影響元ボーン
+                var hasPrev = i - 1 >= 0;
+                var hasNext = i + 1 < virtualEnd;
+                var prev = hasPrev ? influenceSettings.SkirtChains[(i - 1) % chainsCount] : parentTransform;
+                var next = hasNext ? influenceSettings.SkirtChains[(i + 1) % chainsCount] : parentTransform;
+
+                // RotationOffset の計算
+                var middleQuaternion = Quaternion.Slerp(prev.rotation, next.rotation, 0.5f);
+                var rotationDiff = Quaternion.FromToRotation(middleQuaternion * Vector3.up, parentTransform.rotation * Vector3.up);
+
+                // RotationConstraint の設定
+                var rotationConstraint = fakeParent.AddComponent<VRCRotationConstraint>();
+                rotationConstraint.RotationAtRest = parentTransform.localEulerAngles;
+                rotationConstraint.RotationOffset = rotationDiff.eulerAngles;
+                if (hasPrev) rotationConstraint.Sources.Add(new VRCConstraintSource(prev, 0.5f, Vector3.zero, Vector3.zero));
+                if (hasNext) rotationConstraint.Sources.Add(new VRCConstraintSource(next, 0.5f, Vector3.zero, Vector3.zero));
+                rotationConstraint.GlobalWeight = 1.0f;
+                rotationConstraint.IsActive = true;
+            }
+
+            Object.DestroyImmediate(influenceSettings);
+        }
+    }
+}
+
+#endif
---
+++ Editor/ZatoolNdmfPlugin.cs
@@ -0,0 +1,23 @@
+
+#if KZT_NDMF
+
+using nadena.dev.ndmf;
+
+[assembly: ExportsPlugin(typeof(KusakaFactory.Zatools.ZatoolsNdmfPlugin))]
+namespace KusakaFactory.Zatools
+{
+    internal sealed class ZatoolsNdmfPlugin : Plugin<ZatoolsNdmfPlugin>
+    {
+        public override string QualifiedName => "org.kb10uy.zatools";
+        public override string DisplayName => "kb10uy's Various Tools";
+
+        protected override void Configure()
+        {
+            InPhase(BuildPhase.Transforming)
+                .BeforePlugin("nadena.dev.modular-avatar")
+                .Run(new Modules.SkirtInfluenceApplier());
+        }
+    }
+}
+
+#endif
