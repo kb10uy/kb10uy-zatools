650f12c1317e9cba5261e784409171473b42d51a
Yū Kobayashi <kb10uy@kb10uy.org>
2024-09-05 16:32:28 +0900

    Implement incremental search

--- Editor/Inspector/AdHocBlendShapeMix/AdHocBlendShapeMixInspector.cs
+++ Editor/Inspector/AdHocBlendShapeMix/AdHocBlendShapeMixInspector.cs
@@ -84,16 +84,40 @@ namespace KusakaFactory.Zatools.Inspector.AdHocBlendShapeMix
                 var blendShapeNameList = editorWindow.rootVisualElement.Q<ListView>("FieldBlendShapeNames");
                 blendShapeNameList.itemsSource = (IList)_names;
                 blendShapeNameList.makeItem = visualTreeItem.CloneTree;
-                blendShapeNameList.bindItem = (e, i) => e.Q<Label>("LabelName").text = blendShapeNameList.itemsSource[i] as string;
-                blendShapeNameList.selectedIndicesChanged += (idxs) => _boundButton.text = _names[idxs.First()];
-
+                blendShapeNameList.bindItem = (e, i) => OnBindItem(blendShapeNameList, e, i);
+                blendShapeNameList.selectedIndicesChanged += (idxs) => OnSelectionChanged(blendShapeNameList, idxs);
+                // ダブルクリックで閉じられるようにする
                 var doubleClick = new Clickable(() => editorWindow.Close());
                 doubleClick.activators.Clear();
                 doubleClick.activators.Add(new ManipulatorActivationFilter { button = MouseButton.LeftMouse, clickCount = 2 });
                 blendShapeNameList.AddManipulator(doubleClick);
 
+                var searchQueryField = editorWindow.rootVisualElement.Q<TextField>("FieldSearchQuery");
+                searchQueryField.RegisterCallback<ChangeEvent<string>>((ce) => OnUpdateSearchQuery(blendShapeNameList, ce.newValue));
+
                 var initialSelect = _names.IndexOf(_boundButton.text);
                 if (initialSelect != -1) blendShapeNameList.SetSelection(initialSelect);
+            }
+
+            private void OnBindItem(ListView listView, VisualElement itemElement, int index)
+            {
+                itemElement.Q<Label>("LabelName").text = listView.itemsSource[index] as string;
+            }
+
+            private void OnSelectionChanged(ListView listView, IEnumerable<int> selectionIndices)
+            {
+                var selectedIndex = selectionIndices.DefaultIfEmpty(-1).First();
+                if (selectedIndex == -1) return;
+                var boundItems = listView.itemsSource as IList<string>;
+                _boundButton.text = boundItems[selectionIndices.First()];
+            }
+
+            private void OnUpdateSearchQuery(ListView listView, string newQuery)
+            {
+                var trimmedQuery = newQuery.Trim();
+                var filtered = trimmedQuery != "" ? _names.Where((n) => n.Contains(trimmedQuery)).ToList() : _names;
+                listView.ClearSelection();
+                listView.itemsSource = (IList)filtered;
             }
         }
     }
