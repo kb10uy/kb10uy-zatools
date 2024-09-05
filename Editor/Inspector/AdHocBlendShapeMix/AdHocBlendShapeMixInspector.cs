7ea02e82690e328886779bd66a66621c6e39d972
Yū Kobayashi <kb10uy@kb10uy.org>
2024-09-05 14:17:34 +0900

    Use selector

--- Editor/Inspector/AdHocBlendShapeMix/AdHocBlendShapeMixInspector.cs
+++ Editor/Inspector/AdHocBlendShapeMix/AdHocBlendShapeMixInspector.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -44,12 +45,56 @@ namespace KusakaFactory.Zatools.Inspector.AdHocBlendShapeMix
             var item = visualTreeItem.CloneTree();
             ZatoolLocalization.UILocalizer.ApplyLocalizationFor(item);
 
-            var fromDropdown = item.Q<DropdownField>("FieldFromBlendShape");
-            var toDropdown = item.Q<DropdownField>("FieldToBlendShape");
-            fromDropdown.choices = blendShapeNames;
-            toDropdown.choices = blendShapeNames;
+            var fromButton = item.Q<Button>("FieldFromBlendShape");
+            var toButton = item.Q<Button>("FieldToBlendShape");
+            fromButton.clicked += () => UnityEditor.PopupWindow.Show(fromButton.worldBound, new BlendShapeSelector(blendShapeNames, fromButton));
+            toButton.clicked += () => UnityEditor.PopupWindow.Show(toButton.worldBound, new BlendShapeSelector(blendShapeNames, toButton));
 
             return item;
+        }
+
+        internal sealed class BlendShapeSelector : PopupWindowContent
+        {
+            private IList<string> _names;
+            private Button _boundButton;
+
+            internal BlendShapeSelector(IList<string> names, Button boundButton)
+            {
+                _names = names;
+                _boundButton = boundButton;
+            }
+
+            public override void OnGUI(Rect rect)
+            {
+                // Keep empty
+            }
+
+            public override Vector2 GetWindowSize()
+            {
+                return new Vector2(200.0f, 320.0f);
+            }
+
+            public override void OnOpen()
+            {
+                var visualTree = Resources.LoadInspectorVisualTree("AdHocBlendShapeMix/AdHocBlendShapeMixBlendShapeSelector.uxml");
+                var visualTreeItem = Resources.LoadInspectorVisualTree("AdHocBlendShapeMix/AdHocBlendShapeMixBlendShapeItem.uxml");
+
+                visualTree.CloneTree(editorWindow.rootVisualElement);
+
+                var blendShapeNameList = editorWindow.rootVisualElement.Q<ListView>("FieldBlendShapeNames");
+                blendShapeNameList.itemsSource = (IList)_names;
+                blendShapeNameList.makeItem = visualTreeItem.CloneTree;
+                blendShapeNameList.bindItem = (e, i) => e.Q<Label>("LabelName").text = blendShapeNameList.itemsSource[i] as string;
+                blendShapeNameList.selectedIndicesChanged += (idxs) => _boundButton.text = _names[idxs.First()];
+
+                var doubleClick = new Clickable(() => editorWindow.Close());
+                doubleClick.activators.Clear();
+                doubleClick.activators.Add(new ManipulatorActivationFilter { button = MouseButton.LeftMouse, clickCount = 2 });
+                blendShapeNameList.AddManipulator(doubleClick);
+
+                var initialSelect = _names.IndexOf(_boundButton.text);
+                if (initialSelect != -1) blendShapeNameList.SetSelection(initialSelect);
+            }
         }
     }
 }
