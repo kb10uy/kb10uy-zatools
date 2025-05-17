0566659913a6504026b781784b832b7475063c06
Yū KOBAYASHI <kb10uy@kb10uy.org>
2025-05-17 18:41:01 +0900

    Add ZatoolGizmos

---
+++ Runtime/Utility/ZatoolGizmos.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+namespace KusakaFactory.Zatools.Runtime.Utility
+{
+    internal static class ZatoolGizmos
+    {
+        public static void WithContext(Matrix4x4 matrix, Color color, Action draw)
+        {
+            var currentMatrix = Gizmos.matrix;
+            var currentColor = Gizmos.color;
+
+            Gizmos.matrix = matrix;
+            Gizmos.color = color;
+            draw();
+
+            Gizmos.color = currentColor;
+            Gizmos.matrix = currentMatrix;
+        }
+
+        public static void DrawPseudoCapsule(float radius, float length)
+        {
+            var midHalfLength = length / 2.0f - radius;
+            Gizmos.DrawWireSphere(Vector3.up * midHalfLength, radius);
+            Gizmos.DrawWireSphere(Vector3.down * midHalfLength, radius);
+            Gizmos.DrawLineList(new[] {
+                new Vector3(-radius, -midHalfLength, 0.0f),
+                new Vector3(-radius, midHalfLength, 0.0f),
+                new Vector3(radius, -midHalfLength, 0.0f),
+                new Vector3(radius, midHalfLength, 0.0f),
+                new Vector3(0.0f, -midHalfLength, -radius),
+                new Vector3(0.0f, midHalfLength, -radius),
+                new Vector3(0.0f, -midHalfLength, radius),
+                new Vector3(0.0f, midHalfLength, radius),
+            });
+        }
+    }
+}
