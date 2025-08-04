cb2937aad20aaf9bf379147c152bebfaef3ae205
Yū KOBAYASHI <kb10uy@kb10uy.org>
2025-08-04 10:31:02 +0900

    Simplify preview

--- Editor/Ndmf/Preview/AhnbPreview.cs
+++ Editor/Ndmf/Preview/AhnbPreview.cs
@@ -26,21 +26,11 @@ namespace KusakaFactory.Zatools.Ndmf.Preview
             ComputeContext context
         )
         {
-            // コンポーネント側の値の変更
-            var observedPairs = components.Select((c) => context.Observe(
-                c,
-                (c) => (c.Mask, c.Mode, c.Mask != null && c.Mask.isReadable),
-                (oldPair, newPair) => oldPair == newPair
-            ));
-
-            // ボーンの位置変化
+            // コンポーネント側の値の変更と各ボーンの位置変化を監視
+            var observedParameters = components.Select((c) => context.Observe(c, Ahnb.FixedParameters.FixFromComponent, (op, np) => op == np));
             foreach (var bone in original.bones) context.Observe(bone, (t) => t.worldToLocalMatrix);
 
-            foreach ((var mask, var mode, _) in observedPairs)
-            {
-                if (mask == null || !mask.isReadable) continue;
-                Ahnb.ProcessTest1(proxyed, duplicatedMesh, mask, mode);
-            }
+            foreach (var parameters in observedParameters) Ahnb.Process(proxyed, duplicatedMesh, parameters);
 
             return default;
         }
