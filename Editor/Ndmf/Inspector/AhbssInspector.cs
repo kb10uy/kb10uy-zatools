a903288e17a34376fdd83f61d25a435f60768f5b
Yū KOBAYASHI <kb10uy@kb10uy.org>
2025-08-20 23:18:40 +0900

    Add inspector

---
+++ Editor/Ndmf/Inspector/AhbssInspector.cs
@@ -0,0 +1,23 @@
+using UnityEngine.UIElements;
+using UnityEditor;
+using UnityEditor.UIElements;
+using KusakaFactory.Zatools.Localization;
+using KusakaFactory.Zatools.Runtime;
+
+namespace KusakaFactory.Zatools.Ndmf.Inspector
+{
+    [CustomEditor(typeof(AdHocBlendShapeSplit))]
+    internal sealed class AhbssInspector : ZatoolInspector
+    {
+        protected override VisualElement CreateInspectorGUIImpl()
+        {
+            var visualTree = ZatoolsResources.LoadVisualTreeByGuid("72f4638a40cb5e84dbda7e72a8f92a8b");
+
+            var inspector = visualTree.CloneTree();
+            ZatoolsLocalization.UILocalizer.ApplyLocalizationFor(inspector);
+            inspector.Bind(serializedObject);
+
+            return inspector;
+        }
+    }
+}
