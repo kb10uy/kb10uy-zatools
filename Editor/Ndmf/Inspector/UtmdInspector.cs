5b0a048fe5bdab541d0363d4fe8f15e6d5657333
Yū KOBAYASHI <kb10uy@kb10uy.org>
2025-08-23 15:40:30 +0900

    Add inspector

---
+++ Editor/Ndmf/Inspector/UtmdInspector.cs
@@ -0,0 +1,23 @@
+using UnityEngine.UIElements;
+using UnityEditor;
+using UnityEditor.UIElements;
+using KusakaFactory.Zatools.Localization;
+using KusakaFactory.Zatools.Runtime;
+
+namespace KusakaFactory.Zatools.Ndmf.Inspector
+{
+    [CustomEditor(typeof(UvTileMapDistribution))]
+    internal sealed class UtmdInspector : ZatoolInspector
+    {
+        protected override VisualElement CreateInspectorGUIImpl()
+        {
+            var visualTree = ZatoolsResources.LoadVisualTreeByGuid("fc07b878ef0b41b4e9f5926de7593592");
+
+            var inspector = visualTree.CloneTree();
+            ZatoolsLocalization.UILocalizer.ApplyLocalizationFor(inspector);
+            inspector.Bind(serializedObject);
+
+            return inspector;
+        }
+    }
+}
