277e9392c04e1def130518ca2a1e648d0042cdfa
Yū KOBAYASHI <kb10uy@kb10uy.org>
2026-02-24 23:01:02 +0900

    Add localization, helpbox

--- Editor/Ndmf/Inspector/AhmsInspector.cs
+++ Editor/Ndmf/Inspector/AhmsInspector.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEditor;
 using UnityEditor.UIElements;
@@ -9,6 +10,8 @@ namespace KusakaFactory.Zatools.Ndmf.Inspector
     [CustomEditor(typeof(AdHocMeshSplit))]
     internal sealed class AhmsInspector : ZatoolsInspector
     {
+        private HelpBox _warningHelpBox;
+
         protected override VisualElement CreateInspectorGUIImpl()
         {
             var visualTree = ZatoolsResources.LoadVisualTreeByGuid("f357496904123fa43985fe71a6eb2490");
@@ -17,7 +20,18 @@ namespace KusakaFactory.Zatools.Ndmf.Inspector
             ZatoolsLocalization.UILocalizer.ApplyLocalizationFor(inspector);
             inspector.Bind(serializedObject);
 
+            var maskTexture = inspector.Q<ObjectField>("FieldMask");
+            maskTexture.RegisterValueChangedCallback((e) => OnMaskChanged(e.newValue as Texture2D));
+            _warningHelpBox = inspector.Q<HelpBox>("MaskTextureWarning");
+
+            OnMaskChanged(maskTexture.value as Texture2D);
+
             return inspector;
+        }
+
+        private void OnMaskChanged(Texture2D texture)
+        {
+            _warningHelpBox.style.display = texture != null && !texture.isReadable ? DisplayStyle.Flex : DisplayStyle.None;
         }
     }
 }
