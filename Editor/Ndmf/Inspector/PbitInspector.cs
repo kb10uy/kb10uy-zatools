965ea985df6c5e99d5cfe38403e7e1434ced0524
Yū KOBAYASHI <kb10uy@kb10uy.org>
2026-08-14 19:50:38 +0900

    Add inspector

---
+++ Editor/Ndmf/Inspector/PbitInspector.cs
@@ -0,0 +1,23 @@
+using UnityEngine.UIElements;
+using UnityEditor;
+using UnityEditor.UIElements;
+using KusakaFactory.Zatools.Localization;
+using KusakaFactory.Zatools.Runtime;
+
+namespace KusakaFactory.Zatools.Ndmf.Inspector
+{
+    [CustomEditor(typeof(PBIgnoredTransform))]
+    internal sealed class PbitInspector : ZatoolsInspector
+    {
+        protected override VisualElement CreateInspectorGUIImpl()
+        {
+            var visualTree = ZatoolsResources.LoadVisualTreeByGuid("b4856f88b97743f4588284119aebd521");
+
+            var inspector = visualTree.CloneTree();
+            ZatoolsLocalization.UILocalizer.ApplyLocalizationFor(inspector);
+            inspector.Bind(serializedObject);
+
+            return inspector;
+        }
+    }
+}
