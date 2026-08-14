0e23adf591029752b4c2c90c9a8afbd8dc5d8507
Yū KOBAYASHI <kb10uy@kb10uy.org>
2026-08-14 19:50:24 +0900

    Add PBIgnoredTransform component

---
+++ Runtime/PBIgnoredTransform.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace KusakaFactory.Zatools.Runtime
+{
+    [AddComponentMenu("KusakaFactory/Zatools Ignore Self from PB")]
+    [Icon("Packages/org.kb10uy.zatools/Resources/Icon.png")]
+    [HelpURL("https://zatools.kb10uy.dev/ndmf-plugin/pb-ignored-transform/")]
+    public class PBIgnoredTransform : ZatoolsComponent
+    {
+    }
+}
